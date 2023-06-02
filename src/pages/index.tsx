fe411c42cd5a55acae599d60767c5ebfdca4ee6c
faforus <filip.wielechowski@gmail>
2023-06-02 23:37:19 +0200

    position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,9 @@ import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Head from "next/head";
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    const setFullHeight = () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    setFullHeight();
-    window.addEventListener("resize", setFullHeight);
-
-    return () => window.removeEventListener("resize", setFullHeight);
-  }, []);
   return (
     <Fragment>
       <Head>
