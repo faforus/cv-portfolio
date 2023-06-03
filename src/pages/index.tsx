8981936d399ad4f070a9e93a58c881409a95ab6f
faforus <filip.wielechowski@gmail>
2023-06-03 09:42:55 +0200

    head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,26 @@ export default function Home() {
       <Head>
         <link rel="icon" type="image/png" href="/icon.png" />
         <title>Filip Wielechowski</title>
+
+        <meta
+          property="description"
+          content="Filip Wielechowski - React Developer Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Filip Wielechowski - React Developer Portfolio"
+        />
+        <meta
+          name="keywords"
+          content="filip wielechowski react developer javascript typescript nextjs next.js redux tailwind postcss git"
+        />
+        <meta property="og:image" content="image/fbogimage.jpg" />
+        <meta property="og:image:secure_url" content="image/fbogimage.jpg" />
+        <meta property="og:title" content="Filip Wielechowski Portfolio" />
+        <meta property="og:url" content="https://wielechowski.me/" />
+        <meta property="og:type" content="website" />
+
+        <link rel="apple-touch-icon" href="image/appletouch.jpg" />
       </Head>
       <div className="bgColor textOneColor h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#E8AA42]/80">
         <Header />
