e62c3ab34dbd73eab6a06d79c788cadb8d68471b
faforus <filip.wielechowski@gmail>
2023-06-03 12:44:33 +0200

    text update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -29,27 +29,33 @@ export default function About({}: Props) {
           viewport={{ once: true }}
           src="/image/fifisimba.jpg"
           alt="Filip Wielechowski - React Developer Portfolio / TypeScript / JavaScript / React / Next.js / Redux / Tailwind / PostCSS / Git"
-          className="hidden md:block mb-5 md:mb-0 mt-16 md:mt-0 flex-shrink-0 w-[78px] h-[117px] md:w-[312px] md:h-[470px] rounded-lg"
+          className="hidden md:block mb-5 md:mb-0 mt-16 md:mt-0 flex-shrink-0 w-[395px] h-auto rounded-lg"
         />
         <div className="space-y-10 px-0 md:px-10">
           <p className="text-xs md:text-base max-w-[500px] tracking-wide text-justify textTwoColor">
-            Over many years, photography has been my passion. Over the previous
-            few, it started providing me with rewarding but inconsistent income.
-            Seeking greater stability and to maintain my enthusiasm, I&apos;ve
-            decided to transition to programming, currently focusing on
+            Photography has been my long-standing passion, providing rewarding
+            yet inconsistent income. To ensure stability and sustain my
+            enthusiasm, I've transitioned to programming, currently focusing on
             JavaScript and React development.
             <br />
-            <br />I have completed two courses and created two original
-            projects, finding immense excitement in the process. But my learning
-            journey is far from over. I am constantly seeking new challenges and
-            opportunities to expand my knowledge and skills, as I believe that
-            learning is a continuous process that never truly ends. By the time
-            you are reading this, I will have likely learned even more, as I am
-            dedicated to studying and honing my skills every chance I get.{" "}
+            <br />
+            I have successfully completed two extensive courses and
+            independently executed two complex original apps along with other
+            smaller projects. The process of creating these projects has brought
+            me immense excitement and satisfaction. However, my journey of
+            learning continues. Currently, I am engaged in two separate,
+            commercial projects as a member of two different teams. This
+            valuable opportunity not only allows me to apply and enhance my
+            technical skills but also exposes me to the practical aspects of
+            teamwork and collaboration. Throughout these projects, I have been
+            utilizing tools such as GitLab, ClickUp, and Slack, gaining hands-on
+            experience in the collaborative development process of large-scale
+            projects.
             <br />
             <br />I am confident that my combination of creativity, technical
-            skills, and enthusiasm for programming make me a strong candidate
-            for any development position. I am excited about the possibility of
+            skills, and enthusiasm for programming, along with my experience in
+            teamwork and project collaboration, make me a strong candidate for
+            any development position. I am excited about the possibility of
             joining a team that values collaboration, innovation, and continuous
             learning, and I look forward to contributing my skills and expertise
             to help achieve shared goals.
