1b9879366e4da45b93ba2780b3fb265f62f801b0
faforus <filip.wielechowski@gmail>
2023-06-04 10:38:43 +0200

    testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         <section id="hero" className="snap-center">
           <Hero />
         </section>
-        <section id="about" className="snap-center">
+        <section id="about" className="snap-start">
           <About />
         </section>
         {/* <section id="experience" className="snap-center">
